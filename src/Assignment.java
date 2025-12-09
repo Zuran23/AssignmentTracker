61e64747389ad376c095c8f093cc2b68f381f756
JadenMGit <mjaden91@gmail.com>
2025-12-08 19:50:39 -0500

    Update Assignment.java

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,9 +1,11 @@
 //Group5
 public class Assignment extends SchoolItem {
 
+    //Number of points the assignment is worth
     private double points;
+    //Weight of the assignment in the gradebook
     private int weight;
-
+//Constructor: initializes the assignment with its name, points, and weight 
     public Assignment(String name, double points, int weight) {
         super(name);
         this.points = points;
